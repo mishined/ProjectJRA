702b3c20b763cdc38b297c97e5c7ea29bb875dda
Mihaela Nedyalkova <misheton@Mihaelas-MacBook-Pro.local>
2022-06-29 14:19:55 +0100

    update

--- code.py
+++ code.py
@@ -1,8 +1,24 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 import nilearn
-import nibabel
+import nibabel as nib
 
 from nilearn import plotting
 from nilearn import image
 
-plotting.plot_glass_brain("/~/development/ProjectJRA/images/example.nii")   
+# plotting.plot_glass_brain("example.nii")   
+mri_file = 'adexample.nii'
+img = nib.load(mri_file)
 
+# what type is the image
+print(type(img)) 
+# the shape of the image
+print(img.shape)
+
+# get the information of the header
+hdr = img.header
+print(hdr)
+
+# the voxel size (spatial resolution of the image)
+print(img.header.get_zooms())
