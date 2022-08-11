63d7462025e8ade8d83c851f6ce2a66c585752f6
Mihaela Nedyalkova <men22@sussex.ac.uk>
2022-08-11 09:36:07 +0100

    change some arguments defaults to match the project

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
                         help='Latent vector dimension')
     parser.add_argument('--hidden_dim', type=int, default=512,
                         help='Hidden dimension of mapping network')
-    parser.add_argument('--style_dim', type=int, default=64,
+    parser.add_argument('--style_dim', type=int, default=12,
                         help='Style code dimension')
 
     # weight for objective functions
@@ -156,26 +156,26 @@ if __name__ == '__main__':
                         help='Directory for saving metrics, i.e., FID and LPIPS')
 
     # directory for testing
-    parser.add_argument('--result_dir', type=str, default='expr/results',
+    parser.add_argument('--result_dir', type=str, default='/Users/misheton/OneDrive-UniversityofSussex/JRA/results',
                         help='Directory for saving generated images and videos')
-    parser.add_argument('--src_dir', type=str, default='assets/representative/celeba_hq/src',
+    parser.add_argument('--src_dir', type=str, default='/Users/misheton/OneDrive-UniversityofSussex/JRA/Data',
                         help='Directory containing input source images')
-    parser.add_argument('--ref_dir', type=str, default='assets/representative/celeba_hq/ref',
-                        help='Directory containing input reference images')
-    parser.add_argument('--inp_dir', type=str, default='assets/representative/custom/female',
-                        help='input directory when aligning faces')
-    parser.add_argument('--out_dir', type=str, default='assets/representative/celeba_hq/src/female',
-                        help='output directory when aligning faces')
+    # parser.add_argument('--ref_dir', type=str, default='assets/representative/celeba_hq/ref',
+    #                     help='Directory containing input reference images')
+    # parser.add_argument('--inp_dir', type=str, default='assets/representative/custom/female',
+    #                     help='input directory when aligning faces')
+    # parser.add_argument('--out_dir', type=str, default='assets/representative/celeba_hq/src/female',
+                        # help='output directory when aligning faces')
 
     # face alignment
-    parser.add_argument('--wing_path', type=str, default='expr/checkpoints/wing.ckpt')
-    parser.add_argument('--lm_path', type=str, default='expr/checkpoints/celeba_lm_mean.npz')
+    # parser.add_argument('--wing_path', type=str, default='expr/checkpoints/wing.ckpt')
+    # parser.add_argument('--lm_path', type=str, default='expr/checkpoints/celeba_lm_mean.npz')
 
     # step size
-    parser.add_argument('--print_every', type=int, default=10)
-    parser.add_argument('--sample_every', type=int, default=5000)
-    parser.add_argument('--save_every', type=int, default=10000)
-    parser.add_argument('--eval_every', type=int, default=50000)
+    parser.add_argument('--print_every', type=int, default=5)
+    parser.add_argument('--sample_every', type=int, default=5)
+    parser.add_argument('--save_every', type=int, default=1)
+    parser.add_argument('--eval_every', type=int, default=1)
 
     args = parser.parse_args()
     main(args)
