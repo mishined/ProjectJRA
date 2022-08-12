2c1b43b8f65f81f57417a325e06722b758c46b94
Mihaela Nedyalkova <men22@sussex.ac.uk>
2022-08-12 11:26:26 +0100

    corrected dataset and data loader - working

--- new/mri_dataset.py
+++ new/mri_dataset.py
@@ -7,6 +7,7 @@ from PIL import Image
 import numpy as np
 
 import torch
+import pandas as pd
 from torch.utils import data
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
@@ -16,7 +17,7 @@ from torchvision.datasets import ImageFolder
 # listdir() from data_loader
 def load_paths(self, data_path):
     files = []
-    files.append(glob.glob(data_path, 
+    files.append(glob.glob(data_path + "/*/*/*/*/*/*.nii", 
                 recursive = True))
     return files
 
@@ -53,21 +54,31 @@ class MRIDataset(data.Dataset):
 
     def _make_dataset(self, root):
         # get all the files paths
-        fnames = load_paths(root)
+        fnames = load_paths(self, root)
         labels = []
         # get the csv file
         data = self.read_csv(root + "/*.csv")
         # find the corresponding label for all files and add to labels
         for a in range(len(fnames[0])):
+            # print("filename", a)
             id = fnames[0][a][-11:-4]
-            if id[0] == "_":
-                id = id[1:]
+            if id.__contains__('_'):
+                ind = id.find('_')
+                id = id[ind+1:]
+            # print(id)
             index = data.ImageDataID[data.ImageDataID == id].index[0]
             if data.Group[index] == "AD":
                 labels.append("AD")
             else:
                 labels.append("CN")
         return fnames, labels
+    
+    # read a csv file and return the data 
+    def read_csv(self, data_path):
+        file = glob.glob(data_path, 
+                    recursive = True)
+        data = pd.read_csv(file[0])
+        return data
 
     def __len__(self):
         return len(self.samples)
@@ -92,10 +103,26 @@ def get_train_loader(root, which='source', img_size=256,
                              std=[0.5, 0.5, 0.5]),
     ])
 
-    if which == 'source':
-        dataset = ImageFolder(root, transform)
-    else:
-        raise NotImplementedError
-
+    dataset = MRIDataset(root, transform)
     return data.DataLoader(dataset=dataset,
                            batch_size=batch_size)
+
+def get_test_loader(root, img_size=256, batch_size=32):
+    print('Preparing DataLoader for the generation phase...')
+    transform = transforms.Compose([
+        transforms.Resize([img_size, img_size]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5]),
+    ])
+
+    dataset = MRIDataset(root, transform)
+    return data.DataLoader(dataset=dataset,
+                           batch_size=batch_size)
+
+
+test = MRIDataset(root = '/Users/misheton/OneDrive-UniversityofSussex/JRA/Data')
+
+data1 = get_train_loader(root = '/Users/misheton/OneDrive-UniversityofSussex/JRA/Data')
+
+print(data1)
